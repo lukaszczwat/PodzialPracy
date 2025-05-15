1fec6ea2ba381be93588725bac4fdce0ff74d5c3
lukaszwat <lukaszwat@wp.pl>
2025-05-15 13:42:43 +0200

    dodanie testów dla controllera

---
+++ PodzialPracy.Test/TaskControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PodzialPracy.Server.Controllers;
+using PodzialPracy.Server.Serwis;
+
+namespace PodzialPracy.Test
+{
+    public class TaskControllerTest
+    {
+        private readonly Mock<TaskSerwice> _taskServiceMock;
+        private readonly TaskController _taskController;
+
+
+        public TaskControllerTest()
+        {
+            this._taskServiceMock = new Mock<TaskSerwice>();
+            this._taskController = new TaskController(_taskServiceMock.Object);
+        }
+
+        [Fact]
+        public void GetAllTasks_ReturnsOkResult()
+        {
+            _taskServiceMock.Setup(service => service.GetAllTasks()).Returns(new List<Server.Modele.Task>());
+
+            var result = _taskController.GetAllTasks();
+
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+    }
+}
---
+++ PodzialPracy.Test/UserControllerTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PodzialPracy.Server.Controllers;
+using PodzialPracy.Server.Modele;
+using PodzialPracy.Server.Repozytoria;
+using PodzialPracy.Server.Serwis;
+
+namespace PodzialPracy.Test
+{
+    public class UserControllerTest
+    {
+
+        private readonly Mock<IUserRepository> _userRepositoryMock;
+        private readonly UserController _userController;
+
+        public UserControllerTest()
+        {
+
+            _userRepositoryMock = new Mock<IUserRepository>();
+            var userService = new UserService(_userRepositoryMock.Object);
+            _userController = new UserController(userService);
+        }
+
+        [Fact]
+        public void GetAllUsers_ReturnsOkResult_WithUserList()
+        {
+            var expectedUsers = new List<User> { new User(), new User() };
+            _userRepositoryMock.Setup(repo => repo.GetAllUsers()).Returns(expectedUsers);
+
+            var result = _userController.GetAllUsers();
+
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+    }
+}
