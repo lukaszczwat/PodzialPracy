985d1055084d9f9b3e4fc3f62b1c298e44ba7718
lukaszwat <lukaszwat@wp.pl>
2025-05-15 12:33:58 +0200

    dodanie testowania service

---
+++ PodzialPracy.Test/TaskServiceTest.cs
@@ -0,0 +1,46 @@
+﻿using Moq;
+using PodzialPracy.Server.Repozytoria;
+using PodzialPracy.Server.Serwis;
+using System.Reflection;
+
+namespace PodzialPracy.Test
+{
+    public class TaskServiceTest
+    {
+
+        private readonly Mock<ITaskRepository> _taskRepositoryMock;
+        private readonly TaskSerwice _taskService;
+
+        public TaskServiceTest()
+        {
+            
+            this._taskRepositoryMock = new Mock<ITaskRepository>();
+            this._taskService = new TaskSerwice(_taskRepositoryMock.Object);
+
+        }
+
+        [Fact]
+        public void a_GetAllTasks_ReturnsTasks()
+        {
+            // Arrange
+            var mockTask = new List<Server.Modele.Task> { new Server.Modele.Task() { Id = 1, SkalaTrudnosci = 5, Status = Server.Enum.TaskStatus.DoWykonania} };
+            _taskRepositoryMock.Setup(repo => repo.GetAllTasks()).Returns(mockTask);
+
+            // Act
+            var result = _taskService.GetAllTasks();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Single(result);
+            Assert.Equal(5, result.First().SkalaTrudnosci);
+        }
+
+        [Fact]
+        public void b_PrzypiszTask_ThrowException()
+        {
+            var tasks = new List<Server.Modele.Task> { new Server.Modele.Task { Id = 1, SkalaTrudnosci = 5 } };
+
+            Assert.Throws<ArgumentException>(() => _taskService.PrypisanieTask(1, tasks));
+        }
+    }
+}
---
+++ PodzialPracy.Test/UserServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using PodzialPracy.Server.Enum;
+using PodzialPracy.Server.Modele;
+using PodzialPracy.Server.Repozytoria;
+using PodzialPracy.Server.Serwis;
+
+namespace PodzialPracy.Test
+{
+    public class UserServiceTest
+    {
+        private readonly Mock<IUserRepository> _userRepositoryMock;
+        private readonly UserService _userService;
+
+        public UserServiceTest()
+        {
+            this._userRepositoryMock = new Mock<IUserRepository>();
+            this._userService = new UserService(_userRepositoryMock.Object);
+        }
+
+        [Fact]
+        public void GetUsers_ReturnsUserList()
+        {
+            var mockUsers = new List<User> { new User { Id = 1, Imie = "Jan ", Nazwisko = "Kowalski", Typ = UserType.Programista } };
+            _userRepositoryMock.Setup(repo => repo.GetAllUsers()).Returns(mockUsers);
+
+            var result = _userService.GetAllUsers();
+
+            Assert.NotNull(result);
+            Assert.Single(result);
+            Assert.Equal("Jan", result.First().Imie);
+        }
+
+    }
+}
